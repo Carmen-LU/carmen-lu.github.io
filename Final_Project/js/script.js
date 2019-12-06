2a5eb56432d646e49b257378b68413fab00fda0c
Carmen-LU <351167197@qq.com>
2019-12-06 18:53:58 +0800

    update

--- Final_Project/js/script.js
+++ Final_Project/js/script.js
@@ -27,3 +27,39 @@ function show_districts(){
             .addTo(map);
     }
 }
+
+$("button#get_data1").click(function(){
+    var items = [];
+    var i = 0;
+    var airtable_read_endpoint = "https://api.airtable.com/v0/appOkCJ9DXJ5WgGVW/Movie?api_key=keyVgAVOiOdcDu9Eh";
+    var dataSet = [];
+    $.getJSON(airtable_read_endpoint, function(result){
+        $.each(result.records, function(key,value){
+            items = [];
+                items.push(value.fields.电影);
+                items.push(value.fields.上映日期);
+                items.push(value.fields.上映地点);
+                items.push(value.fields.时长);
+                items.push(value.fields.剧情简介);
+                dataSet.push(items);
+                console.log(items);
+        }); // end .each
+        console.log(dataSet);
+
+        $('#table1').DataTable({
+            data: dataSet,
+            retrieve: true,
+            columns:[
+                { title: "电影",
+                  defaultContent:""},
+                { title: "上映日期",
+                  defaultContent:""},
+                { title: "上映地点",
+                  defaultContent:""},
+                { title: "时长",
+                  defaultContent:""},
+                { title: "剧情简介",
+                  defaultContent:""},]
+        });
+    });
+}); 
